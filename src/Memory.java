a83e83c4b6b5fe9a8e74d352eb991df1aac48267
Omar Shaaban <ofahim2002@gmail.com>
2023-05-28 00:21:03 +0300

    Bonus done

--- src/Memory.java
+++ src/Memory.java
@@ -11,6 +11,7 @@ public class Memory {
     public void printMemory(){
         for (int i = 0; i < 2048; i++) {
             System.out.println("Memory Location "+ i + " has the value: " + memory[i]);
+            CPU.s+=("Memory Location "+ i + " has the value: " + memory[i]+"\n");
         }
     }
 }
